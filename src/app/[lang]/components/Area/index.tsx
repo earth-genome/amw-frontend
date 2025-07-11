bf71b649d737e8dd29e0aeb3361f52b70cddd15e
Bernardo Loureiro <bernardo.pacheco@gmail.com>
2025-07-11 14:06:04 -0300

    Start adding card for summary

--- src/app/[lang]/components/Area/index.tsx
+++ src/app/[lang]/components/Area/index.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Logo from "../Nav/logo.svg";
+import React, { useState } from "react";
 import { Tween } from "react-gsap";
-import coverageData from '../../../../../configs/coverage.json';
-import "./style.css";
+import coverageData from "../../../../../configs/coverage.json";
+import style from "./style.module.css";
+import Eye from "@/app/[lang]/components/Icons/Eye";
 
 interface AreaProps {
   dictionary: { [key: string]: any };
@@ -21,35 +20,40 @@ interface Coverage {
   [key: string]: CoverageYear;
 }
 
-
 const Area: React.FC<AreaProps> = ({ dictionary, year }) => {
-  const [showArea, setShowArea] = useState(true);
   const coverage: Coverage = coverageData;
 
-  return showArea ? (
+  return (
     <Tween
       from={{ y: 100, opacity: 0 }}
       to={{ y: 0, opacity: 1 }}
       stagger={0.1}
       ease={"sine.out"}
-      delay={2}
+      delay={0.2}
     >
-      <div className="area">
-        <div className="area-title">
-          {dictionary.coverage.area_affected_in} {year}
+      <div className={style.areaCard}>
+        <div className={style.areaTitle}>
+          {/* FIXME: set programatically and localize */}
+          <div>Whole Amazonia</div>
+          <div>{year}</div>
         </div>
-        <div className="area-km">{ coverage?.[year].km } km<sup>2</sup></div>
-        <div className="area-or">
-          <span className="area-line-left"></span>
-          {dictionary.coverage.or}
-          <span className="area-line-right"></span>
+        <div className={style.areaBody}>
+          <div>{dictionary.coverage.total_area_affected}</div>
+          <div className={style.areaKm}>
+            {coverage?.[year].km} km<sup>2</sup>
+          </div>
+          {/* FIXME: calculate increase and display */}
         </div>
-        <div className="area-acres">
-        { coverage?.[year].acres } {dictionary.coverage.million_acres}
+        {/* FIXME: trigger interaction */}
+        <div className={style.areaFooter}>
+          <div className={style.areaFooterText}>
+            {dictionary.map_ui.see_more_insights}
+            <Eye />
+          </div>
         </div>
       </div>
     </Tween>
-  ) : null;
+  );
 };
 
 export default Area;
