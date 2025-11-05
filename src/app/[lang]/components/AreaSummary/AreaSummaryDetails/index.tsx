6302304abd924fdfe35e0f1be38648bc643b220e
Bernardo Loureiro <bernardo.pacheco@gmail.com>
2025-11-05 19:57:27 -0300

    Remove log

--- src/app/[lang]/components/AreaSummary/AreaSummaryDetails/index.tsx
+++ src/app/[lang]/components/AreaSummary/AreaSummaryDetails/index.tsx
@@ -88,8 +88,6 @@ const AreaSummaryDetails = ({
       />
     );
 
-    console.log(selectedAreaTimeseriesData)
-
   return (
     <div className={style.wrapper}>
       {selectedAreaTimeseriesData?.length ? (
